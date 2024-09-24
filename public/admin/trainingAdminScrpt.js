27a07fdeeb4ae6ec7e8a15ce4a86318d60a28086
Sukanta <sukanta@Sukantas-MBP.lan>
2024-09-24 13:42:49 +0530

    first commit

---
+++ public/admin/trainingAdminScrpt.js
@@ -0,0 +1,181 @@
+function newtrainingChannelInit(){
+    
+    $("#newChannel").html('<div  class="col-xs-12 col-sm-12">\
+    <div class="panel panel-success">\
+          <div class="panel-heading">\
+                <h3 class="panel-title">Register New Partner Channel</h3>\
+          </div>\
+          <div class="panel-body">\
+             <div class="form-group">\
+                    <label>Full Name</label>\
+                        <input type="text"  id="regUserName" class="form-control" placeholder="ei: Sushanta Majumder">\
+                </div>\
+                <div class="form-group">\
+                    <label>Email</label>\
+                        <input type="text"  id="regEmail" class="form-control" placeholder="Email ie hkdhkfhs@gmail.com">\
+                </div>\
+                <div class="form-group">\
+                    <label>Password</label>\
+                        <input type="password"  id="regPassword" class="form-control" placeholder="6 to 18 Chracter">\
+                </div>\
+                <div class="form-group">\
+                    <label>Mobile Number</label>\
+                        <input type="number"  id="regMobile" class="form-control" placeholder=" 10 Digit Number">\
+                </div>\
+                <div class="form-group">\
+                    <label>Channel</label>\
+                        <select id="channelRoot" class="form-control" required="required">\
+                            <option value="">Select Channel</option>\
+                            <option value="A">A</option>\
+                            <option value="B">B</option>\
+                            <option value="C">C</option>\
+                            <option value="D">D</option>\
+                            <option value="E">E</option>\
+                            <option value="F">F</option>\
+                            <option value="G">G</option>\
+                            <option value="H">H</option>\
+                            <option value="I">I</option>\
+                            <option value="J">J</option>\
+                            <option value="K">K</option>\
+                            <option value="L">L</option>\
+                            <option value="M">M</option>\
+                            <option value="N">N</option>\
+                        </select>\
+                </div>\
+                <button onclick="createNewTrainingChannel()" type="button" class="btn btn-primary">Register</button>\
+          </div>\
+    </div>\
+</div>')
+}
+
+
+function createNewTrainingChannel(){
+    var regEmail=$("#regEmail").val().replace(/\s/g, '');
+    var regPassword=$("#regPassword").val();
+    var regUserName=$("#regUserName").val();
+    var regMobile=$("#regMobile").val();
+    var channelRoot=$("#channelRoot").val();
+    // var regPan=$("#regPan").val().toUpperCase().replace(/\s/g, '');
+
+    var reg = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/; 
+
+   
+
+      if (reg.test(regEmail) == false) 
+          {
+              alert('Invalid Email Address');
+              $("#regEmail").focus();
+              return 
+          }
+          if(regPassword.length < 6){
+            alert('Password Must be 6 to 18 charecter');
+            $("#regPassword").focus()
+            return
+        } 
+
+           if(regUserName.length < 2){
+            alert('Enter Valid Name');
+            $("#regUserName").focus()
+            return
+           }
+         
+
+         if(regMobile.length != 10){
+            alert('Enter Valid Mobile Number');
+            $("#regMobile").focus()
+            return
+         }
+        //  if(regPan.length != 10){
+        //     alert('Enter Valid PAN Number');
+        //     $("#regPan").focus()
+        //     return
+        //  }
+         if(!channelRoot.length){
+            alert('Select Root Channel');
+            $("#channelRoot").focus()
+            return
+         }
+
+         ///////Check Exist//////////
+
+         $.post('/admin/trainingCheckuserexist',{channelRoot:channelRoot,regEmail:regEmail},function(data){
+           if(!data){
+            /////////Save New Partner//////
+            $.post('/admin/newTrainingPartner',{
+                regEmail:regEmail,
+                regPassword:regPassword,
+                regUserName:regUserName,
+                regMobile:regMobile,
+                channelRoot:channelRoot
+            },function(reg){
+                alert("Registration  Success")
+            })
+
+           }else{
+            alert("Already Register With Us")
+           }
+         })
+}
+
+function setTrainingStartUserid(){
+    var id=window.prompt();
+    if(id){
+         $.post('/admin/SetTrainingUserID',{id:id},function(data){
+                if(data){
+                    alert("Set Start User ID UserID to"+ data);
+                }
+            })
+    }
+ }
+
+
+ function activationPinGenerate(){
+    $("#view").html('<div class="panel panel-info" style="padding: 5px;">\
+        <div class="panel-heading"  style="margin-top: 10vh;">\
+              <h3 class="panel-title">Generate Activation Pin</h3>\
+        </div>\
+        <div class="panel-body">\
+                <div class="form-group">\
+                    <label class="sr-only" for="">User ID</label>\
+                    <input type="text" class="form-control" id="pinUserID" placeholder="RR-1003">\
+                </div>\
+                <div class="form-group">\
+                    <label class="sr-only" for="">Amount</label>\
+                    <input type="text" class="form-control" id="feeAmount" placeholder="Tuition Fee">\
+                </div>\
+                <button id="activeBtn" onclick="generatePin()" type="submit" class="btn btn-primary">Submit</button>\
+        </div>\
+     </div>')
+ }
+
+ function generatePin(){
+   
+    var tutionFee= $("#feeAmount").val().trim();
+    var userID= $("#pinUserID").val().trim().toUpperCase();
+   
+
+    if(Number(tutionFee) < 1500){
+        alert('Worng Tution Fee');
+        $("#feeAmount").focus()
+        return
+    } 
+
+     
+       $("#activeBtn").css({"display":"none"});
+    $.post('/admin/createActivationKey',{userID:userID,tutionFee:tutionFee},function(data){
+        if(data){
+            $("#pinUserID").val(data)
+        }else{
+            alert("worng User ID")
+            $("#activeBtn").css({"display":"none"}); 
+        }
+    })
+ }
+
+ function trainingFee(){
+    $.post('/admin/activationAmountAdjust',{},function(data){
+        console.log(data);
+    })
+    
+ }
+                
---
+++ routes/training.js
@@ -0,0 +1,379 @@
+var express = require('express');
+var router = express.Router();
+var dbCon = require('../module/db/con');
+var db=require('../module/db/bdModule')
+var auto_incriment=require('../module/db/autoIncriment');
+var dotenv=require('dotenv').config();
+const moment=require('moment');
+
+const bcrypt = require('bcrypt');
+const { ExplainVerbosity } = require('mongodb');
+const saltRounds = 10;
+
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+    try {
+      // await dbCon.connectDB();
+      // await dbCon.closeDB();
+      //console.log(req.query)
+      if(req.query.side && req.query.refid){
+       var sponsID= req.query.refid
+       var sponsSide =req.query.side
+       
+      }else{
+        var sponsID= "";
+        var sponsSide = "";
+      }
+      var allredylogin=req.cookies.traningUserID;
+      res.render('training/trainingUser',{allredylogin:allredylogin,sponsID:sponsID,sponsSide:sponsSide})
+    }catch (error) {
+      console.log(error);
+      return error;
+    }
+    
+  });
+
+  router.post('/loginprocess', async function(req, res, next) {
+    try {
+      await dbCon.connectDB();
+      const user= await db.traininguser.findOne({email:req.body.loginEmail})
+      ///console.log(user);
+      await dbCon.closeDB();
+      if(user){
+        bcrypt.compare(req.body.loginPassword,user.password, async function(err,match){
+          if(match){
+            res.cookie("traningUserID", user.userID, { maxAge:  24 * 60 * 60 * 1000 });
+            res.json(user);
+          }else{
+            res.send(null);
+          }
+        })
+      }else{
+        res.send(null);
+      }
+      
+      
+    }catch (error) {
+      console.log(error);
+      return error;
+    }
+  
+  });
+
+  router.post('/logout', async function(req, res, next) {
+    res.clearCookie("traningUserID");
+    res.send("ok")
+  
+  })
+
+
+
+
+  router.post('/GetUser', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const user= await db.traininguser.findOne({userID:req.body.userID});
+    await dbCon.closeDB();
+    //console.log("check done")
+    res.json(user)
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+
+
+  
+  
+  router.post('/checkExistuser', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const user= await db.traininguser.findOne({$or: [{mobile:req.body.mobileNo},{email:req.body.email}]});
+    await dbCon.closeDB();
+    //console.log("check done")
+    res.json(user)
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+  router.post('/newregister', async function(req, res, next) {
+    try {
+    console.log(req.body)
+    bcrypt.hash(req.body.password, saltRounds, async function(err, hash) {
+
+      auto_incriment.auto_incriment("trainingUserID").then(async function(inc_val){
+        const parentID = await  findParent(req.body.refID, req.body.parentSide)
+        console.log(parentID)
+        await dbCon.connectDB();
+        const user = await db.traininguser.findOne({userID:parentID}) ;
+        if(req.body.parentSide=="L"){
+          var rootID=''+user.rootID+'-'+1+'';
+        }else{
+          var rootID=''+user.rootID+'-'+2+'';
+        }
+
+         
+          const register= await db.traininguser({
+            userName:req.body.userName,
+            userID:'RR-'+inc_val+'',
+            rootID:rootID,
+            parentID:parentID,
+            directParentID:req.body.refID,
+            parentSide:req.body.parentSide,
+            activationPin:"",
+            email:req.body.email,
+            password:hash,
+            mobile:req.body.mobileNo,
+            varyficatinStatus:"NotVerify"
+          })
+          await register.save();
+          await dbCon.closeDB();
+          res.send("ok");
+      })
+    })
+   
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+  async function findParent(refID,Side){
+    var parentID="";
+    var id=refID;
+    try {
+      await dbCon.connectDB();
+   
+      for ( var i=0; i<1000000000; i++) {
+        const user = await db.traininguser.findOne({parentID:id,parentSide:Side}) ;
+        if(!user){
+          parentID=id;
+          i= 1000000000+10;
+          break;
+        }
+        id=user.userID;
+       
+        
+      }
+      await dbCon.closeDB();
+
+      return parentID;
+      
+    } catch (error) {
+    console.log(error);
+    return error;
+  }
+
+  }
+
+  router.post('/activateAccountUser', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const user= await db.traininguser.findOne({activationPin:req.body.activationPinNo,userID:req.body.userID});
+    if(user){
+      const use= await db.traininguser.findOneAndUpdate({userID:req.body.userID},{$set:{
+        varyficatinStatus:"Verify",
+        activationDate: new Date()
+      }});
+      await dbCon.closeDB();
+      res.send("ok");
+    }else{
+      await dbCon.closeDB();
+      res.send("worng");
+    }
+    //console.log("check done")
+    res.json(user)
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+  router.post('/getGeonologyNode', async function(req, res, next) {
+    try {
+      console.log(req.body)
+    await dbCon.connectDB();
+    const root= await db.traininguser.findOne({userID:req.body.userID});
+    const rootLeft= await db.traininguser.findOne({parentID:req.body.userID,parentSide:"L"});
+    const rootRight= await db.traininguser.findOne({parentID:req.body.userID,parentSide:"R"});
+    await dbCon.closeDB();
+    var left = "";
+    var right = "";
+    var leftName = "";
+    var rightName = "";
+    var leftVeryfy = "";
+    var rightVeryfy = ""
+    if(rootLeft){
+      left=rootLeft.userID;
+      leftVeryfy=rootLeft.varyficatinStatus;
+      leftName=rootLeft.userName;
+    }
+    if(rootRight){
+      right=rootRight.userID;
+      rightVeryfy=rootRight.varyficatinStatus;
+      rightName=rootRight.userName;
+    }
+    //console.log(root)
+    res.json({
+      root:root.userID,
+      rootName:root.userName,
+      rootLeft:left,
+      leftName:leftName,
+      rootRight:right,
+      rightName :rightName,
+      rootVerify:root.varyficatinStatus,
+      leftVeryfy:leftVeryfy,
+      rightVeryfy:rightVeryfy
+    });
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+  router.post('/generalData', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const user = await db.traininguser.findOne({userID:req.body.userID});
+    const leftCount = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-1.*' , $options: 'i' } } );
+    const rightCount = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-2.*' , $options: 'i' } } );
+    const leftVerify = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-1.*' , $options: 'i' }, varyficatinStatus:"Verify" } );
+    const rightVerify = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-2.*' , $options: 'i' }, varyficatinStatus:"Verify" } );
+    await dbCon.closeDB();
+    res.json({
+      leftCount:leftCount,
+      rightCount:rightCount,
+      leftVerify:leftVerify,
+      rightVerify:rightVerify
+    })
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+
+router.post('/mydirect', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const direct = await db.traininguser.countDocuments({directParentID:req.body.userID});
+    const directVerify = await db.traininguser.countDocuments({directParentID:req.body.userID, varyficatinStatus:"Verify"});
+    const myDirect = await db.traininguser.find({directParentID:req.body.userID, varyficatinStatus:"Verify"});
+
+    await dbCon.closeDB();
+    res.json({
+      direct:direct,
+      directVerify:directVerify,
+      myDirect:myDirect
+    })
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+  router.post('/earningData', async function(req, res, next) {
+    try {
+    await dbCon.connectDB();
+    const user = await db.traininguser.findOne({userID:req.body.userID});
+    const direct = await db.traininguser.countDocuments({directParentID:user.userID, varyficatinStatus:"Verify" } );
+    const leftVerify = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-1.*' , $options: 'i' }, varyficatinStatus:"Verify" } );
+    const rightVerify = await db.traininguser.countDocuments({rootID: { $regex: '.*' + user.rootID + '-2.*' , $options: 'i' }, varyficatinStatus:"Verify" } );
+    const distingDate = await db.traininguser.distinct("activationDate");
+    console.log(distingDate)
+    await dbCon.closeDB();
+    var pecentage10 = Number(user.activationAmt) * 10/100;
+    var directAmt= Number(pecentage10) * direct
+    if(leftVerify!=rightVerify){
+       if(leftVerify > rightVerify){
+        var match=rightVerify;
+       }else{
+        var match=leftVerify;
+       }
+    }else{
+      var match=leftVerify;
+    }
+    
+  //  const data = await dailyCaping("2024-08-28",user.rootID)
+  //  let { left, right} = await data;
+  //  console.log( data.left, data.right)
+
+  //  var diffa= moment().diff(moment(user.regdate),'days');
+  //  console.log("Diffrence",diffa)
+  //  for(var i=1; i<diffa+1; i++){
+  //   var new_date = moment(user.regdate).add(i, 'days');
+  //   const data = await dailyCaping(new_date,user.rootID)
+  //  let { left, right} = await data;
+  //  console.log( "L",left, "R",right)
+  //  }
+
+   var startdate = moment(user.activationDate).format("L");
+   var new_date = moment(user.activationDate).add(5, 'days');
+   // console.log("active data",user.activationDate,startdate,new_date,moment(new_date))
+
+  //////////Check 10 Condicion of Binary system//////
+
+    var pairMatchAmt = pecentage10 * Number(match)
+
+    var totalEarning= Number(pairMatchAmt) + Number(directAmt);
+    res.json({
+      invtAmt:user.activationAmt,
+      direct:direct,
+      directAmt:directAmt,
+      leftVerify:leftVerify,
+      rightVerify:rightVerify,
+      pairMatch:match,
+      pairMatchAmt:pairMatchAmt,
+      totalEarning:totalEarning
+    })
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
+  
+  });
+
+   async function dailyCaping(date,rootID){
+    try {
+     var out={left:0, right:0};
+     var StartTime = "";
+     var EndTime = ""; 
+     
+     await dbCon.connectDB();
+     StartTime = moment(date).startOf('day').utc();
+     EndTime = moment(date).endOf('day').utc();
+     console.log("StartTime",StartTime,"EndTime",EndTime,"rootID",rootID)
+    const leftVerify = await db.traininguser.countDocuments({
+      rootID: { $regex: '.*' + rootID + '-1.*' , $options: 'i' }, 
+      varyficatinStatus:"Verify",
+      activationDate: { $gte: StartTime.toDate(), $lte: EndTime.toDate() },
+    } );
+    const rightVerify = await db.traininguser.countDocuments({
+      rootID: { $regex: '.*' + rootID + '-2.*' , $options: 'i' },
+       varyficatinStatus:"Verify" ,
+       activationDate: { $gte: StartTime.toDate(), $lte: EndTime.toDate() },
+      } );
+    out={left:leftVerify, right:rightVerify}
+      return out;
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+
+}
+
+
+
+
+module.exports = router;
